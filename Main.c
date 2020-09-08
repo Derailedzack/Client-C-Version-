bc0365d749985f33bb5734f05e373bcde433b5aa
Derailedzack <36719411+Derailedzack@users.noreply.github.com>
2020-09-07 21:17:18 -0400

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,13 +1,43 @@
 
+#include"Main.h"
+#include "EntityLoader.h"
 #include"Client.h"
-#include"SDL2_Window.h"
+
 #include<stdio.h>
-#include<SDL.h>
-int main(int argc, char* argv[]) {
-	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "SDL 2 Error", SDL_GetError(), NULL);
+#include"GDIDisplay.h"
+#include"SDL2_Window.h"
+#include "Player.h"
+#include"LuaUtils.h"
+#include"LevelLoader.h"
+#include <signal.h>
+//Todo:Find a crash reporting library if necessary implement it myself
+//Todo:Find a discord library
+//I'm not sure why I have quite a few of duplicated code
+//That should have been caught and fixed
+
+//#include <xkeycheck.h>
+#include "CuteSound.h"
+#include"TestLevel.h"
+#undef main
+LevelData data;
+bool net_test;
+FILE* client_data_log;
+FILE* file;
+
+char padding[600];
+//const char* data[25+16];
+void segv_handler(int signal){
+	exit(-223);
 }
-	SDL2_CreateWindow(0, 0);
-	printf("Version %i.%i.%i-(%s)", GetClientVersion().major, GetClientVersion().minor, GetClientVersion().patch, GetClientVersion().Build_Type);
+int main(int argc, char* argv[]) {
+
+	SDL_Init(SDL_INIT_EVERYTHING);
+	Player_init();
+	Collsion_init();
+	
+	LoadLevel();
+	
+	Client_Logger_Log(DEBUG,"Test!");
+	
 	return 0;
 }
